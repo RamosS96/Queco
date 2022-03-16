5791a6b9120abaae2c08437c866739013d0eaec6
Sebastian Ramos <sebastianalejandroramos96@gmail.com>
2022-03-16 18:17:40 -0300

    correccion de fetch, agregado de recipes

--- js/main.js
+++ js/main.js
@@ -1,33 +1,12 @@
 let ingredientsList = [];
+let recipesList = [];
+let newIngredientList = [];
 
-fetch('./js/ingredients.json')
-    .then(response => response.json())
-    .then(data => {
-        fruits.push(...data.ingredients.fruits);
-        vegetables.push(...data.ingredients.vegetables);
-        mchicken.push(...data.ingredients.mchicken);
-        mcow.push(...data.ingredients.mcow);
-        mfish.push(...data.ingredients.mfish);
-        mpork.push(...data.ingredients.mpork);
-        legume.push(...data.ingredients.legume);
-        misc.push(...data.ingredients.misc);
-        generateLists(fruits, fruitsList);
-        generateLists(vegetables, vegetablesList);
-        generateLists(mcow, mealcowList);
-        generateLists(mchicken, mealchickenList);
-        generateLists(mfish, mealfishList);
-        generateLists(mpork, mealporkList);
-        generateLists(legume, legumeList);
-        generateLists(misc, miscList);
-        console.log(fruits[1]);
-        console.log(fruits);
-        function addToList(id, group) {
-            let ingredientToAdd = group.find(el => el.id == id.id);
-            ingredientsList.push(ingredientToAdd);
-        }
-        addToList(f10,fruits);
-        console.log(ingredientsList);
-    });
+const cakes = [];
+const meals = [];
+const salads = [];
+const dips = [];
+
 const fruits = [];
 const vegetables = [];
 const mchicken = [];
@@ -47,52 +26,95 @@ const legumeList = document.getElementById('ing-form-legume');
 const miscList = document.getElementById('ing-form-misc');
 
 
+const ingredients = [];
 
-function generateLists(array, group) {
-    group.innerHTML = '';
-    array.forEach(id => {
-        const div = document.createElement('button');
-        div.value = id.id;
-        div.id = id.id;
-        div.innerText = id.desc;
-        div.classList = '';
-        group.append(div);
+
+fetch('./js/ingredients.json')
+    .then(resultado => resultado.json())
+    .then(ingredients => {
+        fruits.push(...ingredients.ingredients.fruits);
+        vegetables.push(...ingredients.ingredients.vegetables);
+        mchicken.push(...ingredients.ingredients.mchicken);
+        mcow.push(...ingredients.ingredients.mcow);
+        mfish.push(...ingredients.ingredients.mfish);
+        mpork.push(...ingredients.ingredients.mpork);
+        legume.push(...ingredients.ingredients.legume);
+        misc.push(...ingredients.ingredients.misc);
+        function addToList(id) {
+
+            ingredientsList.push(id);
+            refreshIngredients();
+            console.log(ingredientsList);
+        };
+        function generateLists(array, group) {
+            group.innerHTML = '';
+            array.forEach(id => {
+                const div = document.createElement('button');
+                div.value = id.id;
+                div.id = id.id;
+                div.innerText = id.desc;
+                div.classList = '';
+                group.append(div);
+                let ids = { id: id.id, desc: id.desc };
+                let btns = document.getElementById(ids.id);
+                btns.onclick = () => { addToList(ids);
+                    refreshIngredients(); }
+            })
+        };
+        generateLists(fruits, fruitsList);
+        generateLists(vegetables, vegetablesList);
+        generateLists(mcow, mealcowList);
+        generateLists(mchicken, mealchickenList);
+        generateLists(mfish, mealfishList);
+        generateLists(mpork, mealporkList);
+        generateLists(legume, legumeList);
+        generateLists(misc, miscList);
+        function refreshIngredients() {
+            ingredientsList = [...new Set(ingredientsList)]
+        }
+        
+        
+        
+        
+        //---------------------------------------------Modal y selecciones----------------------------------
+        const modalOpenBtn = document.getElementById('btn-open-modal');
+        
+        
+        function reduceItem(id) {
+            let ingredientToReduce = ingredientsList.find(el => el.id == id.id);
+            let index = newIngredientList.indexOf(ingredientToReduce)
+            newIngredientList.splice(index, 1);
+        
+            Swal.update({
+        
+                html: 'hola'
+            });
+        
+        }
+        
+        
+        console.log(newIngredientList);
+        ingredientsList.forEach(id => {                               
+            newIngredientList.push(`<button class="refresh-swal" id="${id.desc}">${id.desc} </button>`);
+        });
+        const ingredientsInModal = [...new Set(newIngredientList)];
+        ingredientsInModal.forEach(id =>{
+            let btnReduce = document.getElementById(id.desc);
+            btnReduce.onclick = () => {
+                reduceItem(id.id);
+            }
+
+        })
+        modalOpenBtn.onclick = () => {
+            
+            
+            Swal.fire({
+                template: '#my-template',
+                html: `${ingredientsInModal.join('')}`
+            });
+        }
+
     })
-};
-
-
-
-//---------------------------------------------Modal y selecciones----------------------------------
-const modalOpenBtn = document.getElementById('btn-open-modal');
-const formIng = document.getElementById('ing');
-
-
-function reduceItem(id) {
-    let ingredientToReduce = newIngredientList.find(el => el.id == id.id);
-    let index = newIngredientList.indexOf(ingredientToReduce)
-    newIngredientList.splice(index, 1);
-
-    Swal.update({
-
-        html: 'hola'
-    });
-
-}
-const newIngredientList = [];
-modalOpenBtn.onclick = () => {
-    ingredientsList.forEach(id => {
-        let item;
-        item = id.desc;
-        newIngredientList.push(`<button class="refresh-swal" onclick=reduceItem(${id.id})>${item}</button>`);
-    });
-    const ingredientsInModal = [...new Set(newIngredientList)];
-
-    Swal.fire({
-        template: '#my-template',
-        html: `${ingredientsInModal.join('')}`
-    });
-}
-
 
 
 
